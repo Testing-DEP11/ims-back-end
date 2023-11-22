a8b711aea778b1504289a9b357542165dd150017
nadeesha <nadeeshalakshan525@gmail.com>
2023-11-22 20:23:21 +0530

    finalize web services

--- src/main/java/lk/ijse/dep11/ims/to/CourseTO.java
+++ src/main/java/lk/ijse/dep11/ims/to/CourseTO.java
@@ -1,10 +1,24 @@
 package lk.ijse.dep11.ims.to;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import java.io.Serializable;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CourseTO implements Serializable {
 
+    @Null(message = "Course Id should be empty")
     private Integer courseId;
+    @NotBlank(message = "Course name should not be empty")
     private String courseName;
+    @NotNull(message = "Duration cant be empty")
     private Integer durationInMonths;
 }
