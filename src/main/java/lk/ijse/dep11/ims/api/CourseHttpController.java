6c01d4cdda3b285f2b05dcbf0633c1479fbbc32d
nadeesha <nadeeshalakshan525@gmail.com>
2023-11-22 18:19:54 +0530

    test web services-1

--- src/main/java/lk/ijse/dep11/ims/api/CourseHttpController.java
+++ src/main/java/lk/ijse/dep11/ims/api/CourseHttpController.java
@@ -1,11 +1,42 @@
 package lk.ijse.dep11.ims.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/courses")
 @CrossOrigin
 public class CourseHttpController {
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public void createCourse(){
+        System.out.println("createCourse()");
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping(value = "/{course_id}", consumes = "application/json")
+    public void updateCourse(){
+        System.out.println("updateCourse()");
+
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{course_id}")
+    public void deleteCourse(){
+        System.out.println("deleteCourse()");
+
+    }
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{course_id}")
+    public void getCourse(){
+        System.out.println("getCourse()");
+
+    }
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(produces = "application/json")
+    public void getAllCourses(){
+        System.out.println("getAllCourses()");
+
+    }
+
+
 }
---
+++ src/main/java/lk/ijse/dep11/ims/to/CourseTO.java
@@ -0,0 +1,10 @@
+package lk.ijse.dep11.ims.to;
+
+import java.io.Serializable;
+
+public class CourseTO implements Serializable {
+
+    private Integer courseId;
+    private String courseName;
+    private Integer durationInMonths;
+}
